6843035dea9807714f5da90ff77afdd1a5234534
Justin Yip <yipjustin@gmail.com>
2014-09-19 02:19:17 -0700

    PIOTEAM-41 remove creationTime.

--- examples/event_sample.py
+++ examples/event_sample.py
@@ -42,7 +42,6 @@ second_event_data = {
     "anotherProperty" : "value2"
   },
   "eventTime" : "2004-12-13T21:39:45.618Z",
-  "creationTime" : "2014-09-01T21:40:45.123+01:00"
 }
 print("Second Event response")
 print(client.create_event(second_event_data))
