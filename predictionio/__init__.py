a151e0cdfc9f84334cb29c6bb429a829606f5c4b
goodspeed <cacique1103@gmail.com>
2017-09-10 23:12:50 +0900

    fix InvalidArgumentError is not defined
    Closes #19

--- predictionio/__init__.py
+++ predictionio/__init__.py
@@ -47,6 +47,10 @@ class NotFoundError(PredictionIOAPIError):
   pass
 
 
+class InvalidArgumentError(PredictionIOAPIError):
+    pass
+
+
 def event_time_validation(t):
   """ Validate event_time according to EventAPI Specification.
   """
